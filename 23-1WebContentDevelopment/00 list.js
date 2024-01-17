452ce5b08397c841989b522d3b050c5afa3b3cd4
Remi1001100 <guju0706@gmail.com>
2024-01-17 18:20:01 +0100

    [23년도 1학기 웹 콘텐츠 개발]

---
+++ 23-1WebContentDevelopment/00 list.js
@@ -0,0 +1,10 @@
+var restaurants = [
+    {name: "모미지식당", type: "meal", link:"01-1 momiji.html", tag: "momiji"},
+    {name: "까이식당", type: "meal", link:"01-2 kkai.html", tag: "kkai"},
+    {name: "호밀밭", type:"dessert", link:"01-3 homilbat.html", tag: "homil"},
+    {name: "카페아늑", type: "dessert", link: "01-4 cafeaneuk.html", tag: "aneuk"},
+    {name: "연어초밥", type:"meal", link:"01-5 yeoneochobap.html", tag:"yeoneo"},
+    {name: "꾸덕타코", type: "meal", link:"01-6 ggudeoktako.html", tag: "ggudeok"},
+    {name: "카페코지", type:"dessert", link:"01-7 cafecozy.html", tag:"cozy"} 
+]
+
